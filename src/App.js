a0e6002d2c6032ea3ca3e90a18d33f2d12a6fc43
Stepan Usatyuk <stepus53@gmail.com>
2018-05-11 14:41:26 +0300

    add filters

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import "./App.css";
 
 import InputContainer from "./containers/InputContainer";
 import ItemsContainer from "./containers/ItemsContainer";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <div id="container">
-      <h1>To-Do List</h1>
+      <Header />
       <InputContainer />
       <ItemsContainer />
     </div>
---
+++ src/components/Link.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "../App.css";
+
+export default function Link({ active, onClick, children }) {
+  const classes = ["filter"];
+  if (active) {
+    classes.push("filter--active");
+  }
+  return (
+    <button
+      className={classes.join(" ")}
+      onClick={e => {
+        e.preventDefault();
+        onClick();
+      }}
+    >
+      {children}
+    </button>
+  );
+}
+
+Link.propTypes = {
+  active: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired
+};
