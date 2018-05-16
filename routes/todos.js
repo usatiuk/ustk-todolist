5486b4582fdf58cdfb738b0601426a7b0d80f321
Stepan Usatiuk <stepanusatiuk@gmail.com>
2018-05-16 23:44:13 +0300

    styling

--- routes/todos.js
+++ routes/todos.js
@@ -1,25 +1,25 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
 
 const router = express.Router();
 
-const TodoList = mongoose.model("TodoList");
-const Todo = mongoose.model("Todo");
+const TodoList = mongoose.model('TodoList');
+const Todo = mongoose.model('Todo');
 
-const asyncHelper = require("../asyncHelper");
+const asyncHelper = require('../asyncHelper');
 
 // index
 router.get(
-  "/",
+  '/',
   asyncHelper(async (req, res) => {
     const todos = await Todo.find({}).exec();
     res.json(todos);
-  })
+  }),
 );
 
 // create
 router.post(
-  "/",
+  '/',
   asyncHelper(async (req, res) => {
     const { text, listId } = req.body;
     const list = await TodoList.findById(listId);
@@ -28,7 +28,7 @@ router.post(
     list.todos.push(todo.id);
     await list.save();
     res.json({ success: true });
-  })
+  }),
 );
 
 module.exports = router;
