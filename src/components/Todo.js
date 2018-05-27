26226730a2e92054a4f8bf1ebbc442a53f708835
Stepan Usatiuk <stepus53@gmail.com>
2018-05-27 22:50:42 +0300

    animations

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,6 +2,7 @@ import { faTrash, faEdit, faCheck } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as React from 'react';
 import PropTypes from 'prop-types';
+import { animated } from 'react-spring';
 
 class Todo extends React.Component {
   constructor(props) {
@@ -61,40 +62,40 @@ class Todo extends React.Component {
     if (this.state.editing) {
       let input;
       return (
-        <li>
-          <div className="save" onClick={() => this.stopEdit(input.value)}>
+        <animated.li style={this.props.style}>
+          <button className="save" onClick={() => this.stopEdit(input.value)}>
             <FontAwesomeIcon icon={faCheck} />
-          </div>
+          </button>
           <div className={todoClasses.join(' ')}>
-            <input
+            <textarea
               className="todo--input"
-              type="text"
               defaultValue={this.props.todo.text}
               ref={(node) => {
                 input = node;
               }}
             />
           </div>
-        </li>
+        </animated.li>
       );
     }
     return (
-      <li
+      <animated.li
+        style={this.props.style}
         onMouseOver={this.onMouseOver}
         onFocus={this.onMouseOver}
         onMouseOut={this.onMouseOut}
         onBlur={this.onMouseOut}
       >
-        <div className={deleteClasses.join(' ')} onClick={this.props.removeTodo}>
+        <button className={deleteClasses.join(' ')} onClick={this.props.removeTodo}>
           <FontAwesomeIcon icon={faTrash} />
-        </div>
-        <div className={editClasses.join(' ')} onClick={this.startEdit}>
+        </button>
+        <button className={editClasses.join(' ')} onClick={this.startEdit}>
           <FontAwesomeIcon icon={faEdit} />
-        </div>
-        <div className={todoClasses.join(' ')} onClick={this.props.toggleTodo}>
+        </button>
+        <button className={todoClasses.join(' ')} onClick={this.props.toggleTodo}>
           {this.props.todo.text}
-        </div>
-      </li>
+        </button>
+      </animated.li>
     );
   }
 }
@@ -108,6 +109,7 @@ Todo.propTypes = {
   removeTodo: PropTypes.func.isRequired,
   toggleTodo: PropTypes.func.isRequired,
   editTodo: PropTypes.func.isRequired,
+  style: PropTypes.shape({ maxHeight: PropTypes.number.isRequired }).isRequired,
 };
 
 export default Todo;
--- src/containers/InputContainer.js
+++ src/containers/InputContainer.js
@@ -1,39 +1,27 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import Input from '../components/Input';
 import { addTodo } from '../actions';
 import getVisibleTodos from './getVisibleTodos';
 
-const InputContainer = props => (
-  <Input
-    inputBottomBorder={props.inputBottomBorder}
-    onClick={text => props.dispatch(addTodo(text))}
-  />
-);
-
-InputContainer.propTypes = {
-  inputBottomBorder: PropTypes.bool.isRequired,
-  dispatch: PropTypes.func.isRequired,
-};
-
 function mapStateToProps(state) {
   const { list } = state.lists;
   try {
     const listTodos = state.lists.lists[list].todos;
     return {
-      list,
       inputBottomBorder: getVisibleTodos(listTodos, state.visibilityFilter).length !== 0,
     };
   } catch (e) {
     return {
-      list,
       inputBottomBorder: false,
     };
   }
 }
 
-const InputContainerConnected = connect(mapStateToProps)(InputContainer);
+function mapDispatchToProps(dispatch) {
+  return {
+    onClick: text => dispatch(addTodo(text)),
+  };
+}
 
-export default InputContainerConnected;
+export default connect(mapStateToProps, mapDispatchToProps)(Input);
