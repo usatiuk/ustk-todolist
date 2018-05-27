00f71bb78d1130c743928f228e1f1fdf34d47d7c
Stepan Usatiuk <stepanusatiuk@gmail.com>
2018-05-27 15:07:50 +0300

    use list id for routing

--- models/TodoList.js
+++ models/TodoList.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const slugify = require('slugify');
 
 const { Schema } = mongoose;
 
@@ -8,24 +7,8 @@ const TodoListSchema = Schema({
     type: String,
     required: true,
   },
-  slug: {
-    type: String,
-    required: true,
-    lowercase: true,
-  },
   todos: [{ type: Schema.Types.ObjectId, ref: 'Todo' }],
 });
-
-TodoListSchema.pre('validate', function (next) {
-  if (!this.slug) {
-    this.slugify();
-  }
-  next();
-});
-
-TodoListSchema.methods.slugify = function () {
-  this.slug = slugify(this.name);
-};
 
 TodoListSchema.pre('remove', async function () {
   this.todos.forEach(async (todo) => {
@@ -38,7 +21,6 @@ TodoListSchema.methods.toJson = function () {
   return {
     id: this._id.toString(),
     name: this.name,
-    slug: this.slug,
     todos,
   };
 };
