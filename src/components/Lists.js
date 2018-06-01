a537f99db9a46c707deb8a4fcaa476128ec4bdbe
Stepan Usatiuk <stepanusatiuk@gmail.com>
2018-06-01 21:12:42 +0300

    refactor lists

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,61 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import Selector from './Selector';
-import ListActions from './ListActions';
+import ListActionsContainer from '../containers/ListActionsContainer';
+import SelectorContainer from '../containers/SelectorContainer';
 
-export default function Lists({
-  list,
-  lists,
-  onChange,
-  addList,
-  creating,
-  editing,
-  removeList,
-  startCreateList,
-  startEditList,
-  editList,
-  listObjs,
-}) {
-  const selectorProps = {
-    list,
-    lists,
-    creating,
-    editing,
-    onChange,
-    editList,
-    addList,
-    listObjs,
-  };
-  const actionsProps = {
-    startCreateList,
-    removeList,
-    startEditList,
-    list,
-  };
+export default function Lists() {
   return (
     <div id="lists">
-      <ListActions {...actionsProps} />
-      <Selector {...selectorProps} />
+      <ListActionsContainer />
+      <SelectorContainer />
     </div>
   );
 }
-
-Lists.defaultProps = {
-  list: '',
-};
-
-Lists.propTypes = {
-  lists: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.string.isRequired,
-  })).isRequired,
-  list: PropTypes.string,
-  onChange: PropTypes.func.isRequired,
-  addList: PropTypes.func.isRequired,
-  removeList: PropTypes.func.isRequired,
-  editList: PropTypes.func.isRequired,
-  startCreateList: PropTypes.func.isRequired,
-  startEditList: PropTypes.func.isRequired,
-  creating: PropTypes.bool.isRequired,
-  editing: PropTypes.bool.isRequired,
-  listObjs: PropTypes.any.isRequired,
-};
