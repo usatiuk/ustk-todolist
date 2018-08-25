6fea8998a095f456b3aaf4dd8c153eaad0257d2c
Stepan Usatiuk <stepanusatiuk@gmail.com>
2018-08-25 19:01:36 +0300

    remove selector animation

--- react/src/components/lists/Lists.js
+++ react/src/components/lists/Lists.js
@@ -3,7 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import ListActionsContainer from './ListActionsContainer';
-import SelectorContainer from '../todolist/SelectorContainer';
+import SelectorContainer from './SelectorContainer';
 
 function Lists({ userLoaded, listsLoaded }) {
   return (
