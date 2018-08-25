70c2210ee7436d93d61aba9890a29b62d3f31eb1
Stepan Usatiuk <stepanusatiuk@gmail.com>
2018-08-25 19:28:20 +0300

    disable pointer events on todo leaving transition

--- react/src/components/todolist/TodoList.js
+++ react/src/components/todolist/TodoList.js
@@ -16,8 +16,16 @@ export default function TodosContainer({
         native
         items={todos}
         keys={todo => todo.id}
-        from={{ height: 0, borderColor: '#f0f0f0', opacity: 0.7 }}
-        enter={{ height: 60, borderColor: '#f0f0f0', opacity: 1 }}
+        from={{
+          height: 0,
+          borderColor: '#f0f0f0',
+          opacity: 0.7,
+        }}
+        enter={{
+          height: 60,
+          borderColor: '#f0f0f0',
+          opacity: 1,
+        }}
         leave={{
           height: 0,
           borderColor: '#ffffff',
@@ -25,6 +33,7 @@ export default function TodosContainer({
           opacity: 0.3,
           padding: 0,
           margin: 0,
+          pointerEvents: 'none',
         }}
       >
         {todos.map(todo => styles => (
