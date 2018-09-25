24c1c2ed73d0813dfc4a5d68fa1cba3fc4eebd6b
Stepan Usatyuk <usaatyuk@ustk.me>
2018-09-26 00:15:25 +0300

    clean up google oauth check

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ const production = {
   },
   googleOAuth: {
     googleEnabled:
-      process.env.GOOGLE_ENABLED.toUpperCase() === 'TRUE' ? true : false,
+      process.env.GOOGLE_ENABLED.toUpperCase() === 'TRUE',
     googleClientId: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     googleCallback: `${process.env.HOST}/__/users/login/google/callback`,
