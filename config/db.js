b557fe74dbb5d06a24d73031cb9cc975f9b2023a
Stepan Usatiuk <stepus53@gmail.com>
2020-10-14 10:38:05 +0300

    add mongodb connection options

--- config/db.js
+++ config/db.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 const config = require('./');
 
 async function connect() {
-  await mongoose.connect(config.db.uri);
+  await mongoose.connect(config.db.uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 }
 
 async function disconnect() {
