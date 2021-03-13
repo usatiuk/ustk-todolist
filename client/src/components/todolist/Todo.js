c4f38b529ab867047b9ef9727baf7b0705541482
Stepan Usatiuk <stepus53@gmail.com>
2021-03-13 20:16:43 +0300

    update imports for react-spring

--- client/src/components/todolist/Todo.js
+++ client/src/components/todolist/Todo.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import PropTypes from "prop-types";
-import { animated } from "react-spring";
+import { animated } from "react-spring/renderprops";
 import { ButtonBase } from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
